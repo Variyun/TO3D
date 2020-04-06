71e57c79f17fb48a8d357e0f25458101e5050732
Aaron <aaron.chen1@ucalgary.ca>
2020-04-05 19:52:35 -0600

    Read in all images within a directory specified by user and plot detected features

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-helloworld Executable
+to3d_test Executable
 
 1. Prerequisites for Deployment 
 
@@ -24,7 +24,7 @@ in the MathWorks Documentation Center.
 
 Files to Package for Standalone 
 ================================
--helloworld.exe
+-to3d_test.exe
 -MCRInstaller.exe 
     Note: if end users are unable to download the MATLAB Runtime using the
     instructions in the previous section, include it when building your 
--- to3d.m
+++ to3d.m
@@ -1,6 +1,39 @@
 %% TO3D Application Developed by Aaron Chen and Chris Lin
 % Developed for ENGO 559 Digital Imaging Winter 2020
+clc
+% User must enter in the directory containing images and file extension
+fprintf('Welcome to TO3D!\n')
+dir_in = input('Please enter name of directory with images: ', 's');
+type = input('Please enter the file format of your images (ex. png, jpg, tiff): ', 's');
+dir_in = strcat(dir_in, '/*.', type);
+% read directory
+images = dir(dir_in);
+% if nothing is read in, error message
+if isempty(images)
+    fprintf('Error reading files in directory: %s\n', dir_in);
+    fprintf('Press any key to exit.\n');
+    pause;
+    return
+end
 
-fprintf('Hello World\n')
-fprintf('Program finished, press any key to exit.\n');
+for i = 1: length(images)
+    impath = strcat(images(i).folder,'/', images(i).name); 
+    a = imread(impath); 
+    feat = detectHarrisFeatures(rgb2gray(a));
+    plot_feat(a, feat);
+end
+
+fprintf('\nProgram finished, press any key to exit.\n');
 pause;
+return 
+
+%% Function Definitions
+
+% Function to plot images with strongest 50 features 
+function plot_feat(I, feature)
+    figure
+    imshow(I)
+    hold on
+    plot(feature.selectStrongest(50));
+    hold off
+end
