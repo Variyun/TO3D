e181471480911358075c372b8d1ccaf534067a8d
Aaron <aaron.chen1@ucalgary.ca>
2020-04-14 17:45:42 -0600

    Recreate scence using disparity map, almost working

--- to3d.m
+++ to3d.m
@@ -11,7 +11,7 @@
 
 %% Main 
 clc
-% ***** IMPORTANT: UNCOMMENT SECTION BELOW FOR PRODUCTION VERSION *****
+clear all
 % User must enter in the directory containing images and file extension
 % fprintf('Welcome to TO3D!\n')
 % dir_in = input('Please enter name of directory with images: ', 's');
@@ -26,21 +26,50 @@ clc
 %     pause;
 %     return
 % end
-%
-% for i = 1: length(images)
-%     impath = strcat(images(i).folder,'/', images(i).name); 
-%     a = imread(impath); 
-%     feat = detectHarrisFeatures(rgb2gray(a));
-%     plot_feat(a, feat);
+
+% for i = 1:length(images)-1
+%     fprintf('Working on image pair %s & $s .....', images(i).name, images(i+1).name);
+%     impath1 = strcat(images(i).folder,'\', images(i).name); 
+%     impath2 = strcat(images(i+1).folder,'\', images(i+1).name); 
+%     disparity(impath1, impath2, images(i).name, images(i+1).name);
+%     fprintf('DONE\n');
 % end
 
+% TESTING TESTING TESTING
+
+path1 = 'StereoImages\DSC05899.JPG';
+path2 = 'StereoImages\DSC05900.JPG';
+disparity(path1, path2, 'DSC05899.JPG', 'DSC05900.JPG');
+% path1 = 'DSC05644.JPG';
+% path2 = 'DSC05645.JPG';
+% disparity(path1, path2, 'DSC05644.JPG', 'DSC05645.JPG');
+fprintf('DONE\n');
+
+% End of Program 
+% fprintf('\nProgram finished, press any key to exit.\n');
+% pause;
+return 
+
 %% Undistort Images
+function disparity(path1, path2, name1, name2)
 clc
 close all
 
 % Load in image pairs
-left = imread('TestStereo/DSC05899.JPG');
-right = imread('TestStereo/DSC05900.JPG');
+try
+    left = imread(path1);
+catch
+    fprintf("Couldn't open image %s.\nPress any key to close.\n", name1);
+    pause;
+    return
+end
+try
+    right = imread(path2);
+catch
+    fprintf("Couldn't open image %s.\nPress any key to close.\n", name2);
+    pause;
+    return
+end
 
 % Define camera Intrinsic Matrix and Radial Distortion Parameters
 inMatrix=[4066.861 0 0;0 4066.861 0; 2978.91370054375 2030.83585703852 1];
@@ -51,78 +80,80 @@ cameraParams = cameraParameters('IntrinsicMatrix', inMatrix, ...
     'NumRadialDistortionCoefficients', 3);
 
 % Undistort Image
-fprintf('Undistorting Images...')
-[left_undistorted, left_origin] = undistortImage(left, cameraParams);
-[right_undistorted, right_origin] = undistortImage(right, cameraParams);
-fprintf('DONE\n')
-
-% Display undistorted image for testing
-% imshowpair(left, left_undistorted, 'montage')
-% figure 
-% imshowpair(right, right_undistorted, 'montage')
+% NOTE - Images being resized to reduce computation time and machine stress
+left = imresize(left, 0.5);
+right = imresize(right, 0.5); 
+[left_undistorted, ~] = undistortImage(left, cameraParams);
+[right_undistorted, ~] = undistortImage(right, cameraParams);
 
 % Feature Detection
-fprintf('Detecting Features...')
 left_points = detectSURFFeatures(rgb2gray(left_undistorted));
 right_points = detectSURFFeatures(rgb2gray(right_undistorted));
-fprintf('DONE\n')
 
 % Extract Features (currently only the strongest 1000)
-fprintf('Extracting Features...')
-[left_features, left_coords] = extractFeatures(rgb2gray(left_undistorted),left_points.selectStrongest(500));
-[right_features, right_coords] = extractFeatures(rgb2gray(right_undistorted),right_points.selectStrongest(500));
-fprintf('DONE\n')
+[left_features, left_coords] = extractFeatures(rgb2gray(left_undistorted),left_points.selectStrongest(1000));
+[right_features, right_coords] = extractFeatures(rgb2gray(right_undistorted),right_points.selectStrongest(1000));
 
 % Matching Features
-fprintf('Matching Features...')
 indexPairs = matchFeatures(left_features, right_features);
 
-left_match = zeros(size(indexPairs));
-right_match = zeros(size(indexPairs));
+left_match = double(left_coords.Location(indexPairs(:,1),1:2));
+right_match = double(right_coords.Location(indexPairs(:,2),1:2));
 
-% Swap x and y 
-left_match(:,1) = double(left_coords.Location(indexPairs(:,1),2));
-left_match(:,2) = double(left_coords.Location(indexPairs(:,1),1));
+% Compute fundamental matrix and get inliers for true matches 
+% [f, indexPairs] = estimateFundamentalMatrix([left_match(:,2) left_match(:,1)], ...
+%     [right_match(:,2) right_match(:,1)], 'Method', 'RANSAC');
+[f, indexPairs] = estimateFundamentalMatrix(left_match, right_match, 'Method', 'RANSAC');
 
-right_match(:,1) = double(right_coords.Location(indexPairs(:,2),2));
-right_match(:,2) = double(right_coords.Location(indexPairs(:,2),1));
+left_true = left_match(indexPairs, 1:2); 
+right_true = right_match(indexPairs, 1:2); 
 
-% Use RANSAC to find true matches
-[~, inliers] = ransacfithomography(left_match', right_match', 0.01);
-
-% Set true matches
-left_match = left_match(inliers(:), 1:2); 
-right_match = right_match(inliers(:), 1:2); 
-
-fprintf('DONE\n')
-
-% Estimate fundamental matrix 
-f = estimateFundamentalMatrix(left_true, right_true);
+clearvars -except left_true right_true f name1 name2 ...
+    left_undistorted right_undistorted cameraParams
 
 % Estimate rectification parameters
-[t1, t2] = estimateUncalibratedRectification(f, left_match, right_match, size(left_undistorted)); 
-[left_rect, right_rect] = rectifyStereoImages(left_undistorted, right_undistorted, t1, t2);
+[t1, t2] = estimateUncalibratedRectification(f, left_true, right_true, size(left_undistorted)); 
+%[left_rect, right_rect] = rectifyStereoImages(left_undistorted, right_undistorted, t1, t2);
+[left_rect, right_rect] = rectifyStereoImages(left_undistorted, right_undistorted, t1, t2,'OutputView','full');
 
-%show stereo anaglyph 
-stereo = stereoAnaglyph(left_rect, right_rect);
-figure;
-imshow(stereo);
+% Show stereo anaglyph 
+% stereo = stereoAnaglyph(left_rect, right_rect);
+% figure;
+% imshow(stereo);
 
 % Compute disparity map
-left_gray = rgb2gray(left_rect);
-right_gray = rgb2gray(right_rect);
+disparityRange = [-64 64];
+disparityMap = disparitySGM(rgb2gray(left_rect),rgb2gray(right_rect), ...
+     'DisparityRange',disparityRange, 'UniquenessThreshold', 0);
+ 
+% -- UNCOMMENT BELOW TO SAVE IMAGES AND DISPLAY DISPARITY MAP --
+% figure
+% imshow(disparityMap,disparityRange)
+% title('Disparity Map')
+% colormap jet
+% colorbar
 
-disparityRange = [0 48];
-disparityMap = disparitySGM(left_gray,right_gray,'DisparityRange',disparityRange,'UniquenessThreshold',20);
+% name1 = strcat('Rectified\RECT_', name1);  
+% name2 = strcat('Rectified\RECT_', name2);  
+% 
+% imwrite(left_rect, name1);
+% imwrite(right_rect, name2);
 
-figure
-imshow(disparityMap,disparityRange)
-title('Disparity Map')
-colormap jet
-colorbar
+% DisparitySGM very hard on computer for large images - BE CAREFUL
+% imwrite(disparitySGM(rgb2gray(left_rect),rgb2gray(right_rect), ...
+%      'DisparityRange',disparityRange, 'UniquenessThreshold', 0),...
+%      'Disparity\DISP_Pair.jpg');
+ 
+% Calculate 
+[R, t] = cameraPose(f, cameraParams, left_true, right_true);
 
-% End of Program 
-fprintf('\nProgram finished, press any key to exit.\n');
-pause;
-return 
+%calculate stereoParameters
+left_cam = cameraParameters('IntrinsicMatrix', t1*f);
+right_cam = cameraParameters('IntrinsicMatrix', t2*f);
+
+stereoParams = stereoParameters(left_cam, right_cam, R, t);
+xyzPoints = reconstructScene(disparityMap,stereoParams);
+
+end
+
 
